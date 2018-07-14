126b41ea015a5d61ac0e3a96f795c0a38daef9aa
Scott Ammon <35412449+scott-ammon@users.noreply.github.com>
2018-07-14 11:22:19 -0700

    Update Card.js

--- models/Card.js
+++ models/Card.js
@@ -1,17 +1,1 @@
-const mongoose = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
-  tabsPunched: Number,
-  requiredPunches: Number,
-  expirationDate: Number,
-});
-
-const Card = mongoose.model('Card', cardSchema);
-
-module.exports = Card;
-
-Card.findOne({
-  title: 'Fahrenheit 451'
-}).populate('User').exec(function(err, card) {
-  console.log(err, card);
-});
