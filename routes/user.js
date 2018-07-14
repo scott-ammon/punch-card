44e6690008faf2534680552cf7d62e1b616fca7e
William Connelly <williamconnelly.dev@gmail.com>
2018-07-14 11:06:49 -0700

    Added card and restaurant models, added cards to user model

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Restaurant = require('../models/Restaurant');
+const Card = require('../models/Card');
+
+
+
+module.exports = router;
