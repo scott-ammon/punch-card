27cf4d00845fbb67ca5cc269a8f03c4767fe5b7c
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-07-15 22:31:17 -0700

    created componentDidMount method

--- client/src/CardPreview.js
+++ client/src/CardPreview.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 
 class CardPreview extends Component {
-  constructor() {
+  constructor(props) {
+    super(props);
     this.state = {
-      cards: [
-        {
-          name: 'Teriyaki Highway',
-          punches: 4,
-          reqPunches: 9
-        },
-        {
-          name: 'Olive Garden',
-          punches: 5,
-          reqPunches: 6
-        },
-        {
-          name: 'Taco Del Mar',
-          punches: 2,
-          reqPunches: 8
-        }
-      ],
+      cards: [],
     }
+  }
+
+  componentDidMount() {
+    this.State({projects: [
+      {
+        name: 'Teriyaki Highway',
+        punches: 4,
+        reqPunches: 9
+      },
+      {
+        name: 'Olive Garden',
+        punches: 5,
+        reqPunches: 6
+      },
+      {
+        name: 'Taco Del Mar',
+        punches: 2,
+        reqPunches: 8
+      }
+    ]
+  })
   }
 
   render() {
