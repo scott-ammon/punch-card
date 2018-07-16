d232ecae86eeded0ca1e1e6a622894f5dfaa24e2
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-07-16 12:54:09 -0700

    worked on card component jsx

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 class Card extends Component {
   constructor(props) {
@@ -17,19 +18,23 @@ class Card extends Component {
 
   render() {
 
-    let card = this.props.cards.find((card) => {
-          return card.id === parseInt(this.props.match.params.id)
-        })
+    // let card = this.props.cards.find((card) => {
+    //       return card.id === parseInt(this.props.match.params.id)
+    //     })
 
     return (
       <div>
-        <h1>{card.name}</h1>
-        <p>{this.props.card.reqPunches} punches to get a free sandwich</p>
+        <h1>Restaurant Name</h1>
+        <p>8 punches to get a free sandwich</p>
         <div>
           1 2 3 4 5 6 7 8 9 10
         </div>
         <div>
-          <button>Remove Card</button>
+          <input placeholder="enter code to punch"/>
+          <a>Redeem</a>
+        </div>
+        <div>
+          <a>Remove Card</a>
         </div>
       </div>
     )
