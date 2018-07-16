ae96443639cce37a30ba2e1906453b3ef2d619f9
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-07-15 22:15:50 -0700

    created constructor

--- client/src/CardPreview.js
+++ client/src/CardPreview.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class CardPreview extends Component {
+  constructor() {
+    this.state = {
+      cards: [
+        {
+          name: 'Teriyaki Highway',
+          punches: 4,
+          reqPunches: 9
+        },
+        {
+          name: 'Olive Garden',
+          punches: 5,
+          reqPunches: 6
+        },
+        {
+          name: 'Taco Del Mar',
+          punches: 2,
+          reqPunches: 8
+        }
+      ],
+    }
+  }
 
   render() {
     return (
       <h1>My Cards</h1>
+
     )
   }
 }
