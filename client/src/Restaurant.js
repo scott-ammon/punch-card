7d2e205e563477995f8db137ed2398a39aa5f821
Scott Ammon <scott.o.ammon@gmail.com>
2018-07-16 22:20:28 -0700

    fixing base styles on pages

--- client/src/Restaurant.js
+++ client/src/Restaurant.js
@@ -11,36 +11,37 @@ class Restaurant extends Component {
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  componentDidMount() {
+    var restaurant = this.props.restaurants.find((restaurant) => {
+      return restaurant._id === this.props.match.params.id
+    })
+    this.setState({
+      restaurant: restaurant
+    })
+  }
   
   handleSubmit(e) {
       e.preventDefault()
       console.log(this.props.user)
       axios.post("/user/cards", {
         user: this.props.user,
-        restaurant: this.state.restaurant
+        restaurant: this.state.restaurant._id
       }).then(result => {
         console.log(result)
       })
     }
 
   render() {
-    var restaurant = this.props.restaurants.find((restaurant) => {
-      return restaurant._id === this.props.match.params.id
-    })
-
-    this.setState({
-      restaurant: restaurant._id
-    })
-
     return (
       <div className="">
-        <h1>{restaurant.name}</h1>
+        <h1>{this.state.restaurant.name}</h1>
         <div>
           <img src=""/>
         </div>
-        <h4>Location: {restaurant.address}</h4>
-        <h4>{restaurant.genre}</h4>
-        <h4>Card Reward: {restaurant.reward}</h4>
+        <h4>Location: {this.state.restaurant.address}</h4>
+        <h4>{this.state.restaurant.genre}</h4>
+        <h4>Card Reward: {this.state.restaurant.reward}</h4>
         <div>
           <form onSubmit={this.handleSubmit}>
             <Button component={Link} to="/cards" variant="contained" color="primary" type="submit">Add new card!</Button>
