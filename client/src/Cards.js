2d157822cef2af9a6121018a9e1fdedfb8d111df
Matt Carver <mattcarver.wd@gmail.com>
2018-07-17 14:01:38 -0700

    materialui stuff

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CardPreview from './CardPreview';
 import axios from 'axios';
+import SimpleMediaCard from './SimpleMediaCard';
 
 class Cards extends Component {
   constructor(props) {
@@ -10,34 +10,38 @@ class Cards extends Component {
     }
   }
 
-  componentDidMount() {
-    console.log("this.props.user is: ", this.props.user)
-    axios.post("/user/cards/all", {
-      user: this.props.user
-    }).then(result => {
-      console.log(result)
-      this.setState({
-        cards: result
-      })
-    })
-  }
+  // componentDidMount() {
+  //   console.log("this.props.user is: ", this.props.user)
+  //   axios.post("/user/cards/all", {
+  //     user: this.props.user
+  //   }).then(result => {
+  //     console.log(result)
+  //     this.setState({
+  //       cards: result
+  //     })
+  //   })
+  // }
 
   render() {
-
-    let cardsList = this.state.cards.map((card, i) => {
-      return(
-        <CardPreview card={card} />
-      )
-    })
-
-    return (
-      <div>
-        <h1>My Cards</h1>
+    return(
+    // let cardsList = this.state.cards.map((card, i) => {
+      // return(
         <div>
-          {cardsList}
+        <SimpleMediaCard />
         </div>
-      </div>
-    )
+      // )
+    // }
+  )
+
+//
+//     return (
+//       <div>
+//         <h1>My Cards</h1>
+//         <div>
+//
+//         </div>
+//       </div>
+//     )
   }
 }
 
